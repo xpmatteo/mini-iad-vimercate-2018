3764187ac3703aac15fed4ae6a6377881587b611
Matteo Vaccari <vaccari@pobox.com>
2016-10-29 19:11:16 +0200

    add deploy script

---
+++ script/deploy.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -e
+cd "$(dirname "$0")"/..
+
+PEM=../Infrastruttura/iam_amazon.pem
+
+[ -f "$PEM" ] || {
+  echo "$PEM: not found"
+  exit 1
+}
+
+rsync -av \
+  --exclude '.git*' \
+  -e "ssh -i $PEM" \
+  public \
+  ubuntu@54.154.31.160:/www/agileday_new/agileday.it/htdocs/mini/2017/vimercate
+
+
+
